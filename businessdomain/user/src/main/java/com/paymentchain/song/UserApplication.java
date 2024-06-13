76e91d198c29a34529469f6cb7ceaaafba06c6b1
msanchezsevi_meli <manuel.sanchezsevilla@mercadolibre.com.co>
2024-06-12 20:11:35 -0500

    first commit

---
+++ businessdomain/user/src/main/java/com/paymentchain/song/UserApplication.java
@@ -0,0 +1,23 @@
+package com.paymentchain.song;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public WebClient.Builder loadBalancedWebClientBuilder() {
+		return WebClient.builder();
+	}
+}
---
+++ businessdomain/user/src/test/java/com/paymentchain/song/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentchain.song;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
