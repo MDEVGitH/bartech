ee0acf6bf4ed8b949968151ce8981cdfb6e2be4c
MDEVGitH <manuel.sanchezsevilla@mercadolibre.com.co>
2024-06-17 14:59:08 -0500

    adding api gateway

--- businessdomain/user/src/main/java/com/paymentchain/song/UserApplication.java
+++ businessdomain/user/src/main/java/com/paymentchain/song/UserApplication.java
@@ -2,13 +2,12 @@ package com.paymentchain.song;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class UserApplication {
 
 	public static void main(String[] args) {
